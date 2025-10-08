9cc2ada8603029b9c2617db8c27b78c8bda0578d
Ryan Hung <rh.lh.314@gmail.com>
2025-10-07 17:52:45 -0700

    updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,10 @@ public class DeveloperTest {
         assertEquals("Ryan", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("RyanHung", Developer.getGithubId());
+    }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
